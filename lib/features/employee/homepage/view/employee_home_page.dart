7e06fb04bdea39ef85912bbdfd6e9000e6248b53
Maha <mapamungkas38@gmail.com>
2024-05-10 21:01:55 +0700

    style: add pages and routes

---
+++ lib/features/employee/homepage/view/employee_home_page.dart
@@ -0,0 +1,17 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class EmployeeHomePage extends StatefulWidget {
+  const EmployeeHomePage({super.key});
+
+  @override
+  State<EmployeeHomePage> createState() => _EmployeeHomePageState();
+}
+
+class _EmployeeHomePageState extends State<EmployeeHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/features/employee/profile/view/employee_profile_page.dart
@@ -0,0 +1,12 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class EmployeeProfilePage extends StatelessWidget {
+  const EmployeeProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
